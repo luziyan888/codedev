30d8116d742067fc7b869b8e7a0a282f2934f67c
86152 <yxxl_luziyan>
2024-04-01 17:05:42 +0800

    提交订单

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -3,6 +3,14 @@ import { useCartStore } from "@/stores/cartStore";
 
 
 const cartStore = useCartStore()
+
+const singleCheck = (i, selected) => {
+  cartStore.singleCheck(i.skuId, selected)
+}
+
+const allCheck = (selected) => {
+  cartStore.allCheck(selected)
+}
 </script>
 
 <template>
@@ -13,7 +21,7 @@ const cartStore = useCartStore()
           <thead>
             <tr>
               <th width="120">
-                <el-checkbox/>
+                <el-checkbox :model-value="cartStore.isAll" @change="allCheck"/>
               </th>
               <th width="400">商品信息</th>
               <th width="220">单价</th>
@@ -26,7 +34,8 @@ const cartStore = useCartStore()
           <tbody>
             <tr v-for="i in cartStore.cartList" :key="i.id">
               <td>
-                <el-checkbox />
+                <!-- 单选框 以回调函数的方式能实现多传一个参数-->
+                <el-checkbox :model-value="i.selected" @change="(selected) => singleCheck(i,selected)" />
               </td>
               <td>
                 <div class="goods">
@@ -73,11 +82,11 @@ const cartStore = useCartStore()
       <!-- 操作栏 -->
       <div class="action">
         <div class="batch">
-          共 10 件商品，已选择 2 件，商品合计：
-          <span class="red">¥ 200.00 </span>
+          共 {{cartStore.allCount}} 件商品，已选择 {{cartStore.selectedCount}} 件，商品合计：
+          <span class="red">¥ {{cartStore.selectedPrice.toFixed(2)}} </span>
         </div>
         <div class="total">
-          <el-button size="large" type="primary" >下单结算</el-button>
+          <el-button size="large" type="primary" @click="$router.push('/checkout')" >下单结算</el-button>
         </div>
       </div>
     </div>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,13 +3,13 @@ import { ref } from "vue";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from "vue-router";
-import {useUserStore} from '@/stores/user'
+import {useUserStore} from '@/stores/userStore'
 
 const userStore = useUserStore()
 //表单校验(账户名+密码)
 const form = ref({
-    account:'',
-    password:'',
+    account:'xiaotuxian001',
+    password:'123456',
     agree:true
 })
 
